daa2673cdede15d86d6b8448855917fdd484b204
lemesvini <viniciuslemesds@gmail.com>
2025-12-24 15:02:25 -0300

    fix: update profile bug

--- src/routes/auth.routes.ts
+++ src/routes/auth.routes.ts
@@ -5,15 +5,27 @@ import { authenticate } from "../middlewares/auth";
 const authController = new AuthController();
 
 export async function authRoutes(app: FastifyInstance) {
-  app.post("/register", authController.register);
-  app.post("/login", authController.login);
-  app.get("/me", {
-    onRequest: [authenticate],
-    handler: async (request, reply) => authController.me(request as any, reply),
-  });
-  app.patch("/me", {
-    onRequest: [authenticate],
-    handler: async (request, reply) =>
-      authController.updateProfile(request as any, reply),
-  });
+  app.post<{
+    Body: {
+      email: string;
+      username: string;
+      fullName: string;
+      password: string;
+    };
+  }>("/register", authController.register);
+
+  app.post<{
+    Body: { email: string; password: string };
+  }>("/login", authController.login);
+
+  app.get("/me", { onRequest: [authenticate] }, authController.me);
+
+  app.patch<{
+    Body: {
+      bio?: string;
+      profilePictureUrl?: string;
+      fullName?: string;
+      instagramUsername?: string;
+    };
+  }>("/me", { onRequest: [authenticate] }, authController.updateProfile);
 }
